6767174372a93b0cb976e65fca7659b4ee56c881
Mewton Games <mewton.games@gmail.com>
2026-01-29 10:01:33 +0200

    Add player prefs window & wrapper

--- Editor/PlayerPrefsWindow.cs
+++ Editor/PlayerPrefsWindow.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using MewtonGames.DataStorage.Providers;
+using MewtonGames.DataStorage;
 using UnityEditor;
 using UnityEngine;
 
